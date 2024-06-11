0af8524ae8cb1843960955286756a502610c821c
Amit Kumar <amitk@tilli.pro>
2024-06-12 02:29:41 +0530

    added random user data in template

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users');
+router.get("/", function (req, response, next) {
+  let user = {};
+  fetch("https://randomuser.me/api")
+    .then((res) => res.json())
+    .then((res) => {
+      user = res.results[0];
+      console.log("user: ", user)
+      response.render("users", { user: user })
+    });
 });
 
 module.exports = router;
