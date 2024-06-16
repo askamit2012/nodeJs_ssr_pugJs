cbaac6ab2a9e875810c7021d8e698d6f573b0e7a
Amit Kumar <amitk@tilli.pro>
2024-06-17 00:50:13 +0530

    working on to-do page

--- routes/toDo.js
+++ routes/toDo.js
@@ -1,8 +1,19 @@
 var express = require('express')
+var fs = require('fs')
+var path = require('path')
 var router = express.Router()
 
+var fileName = path.join(__dirname + '../../' + '/public/files/tasks.json')
+
 router.get('/', (req, res, next) => {
-    res.render('toDo')
+    console.log('to do router')
+    try {
+        fs.readFile(fileName, 'utf-8', (err, data) => {
+            res.render('toDo', { tasks: data })
+        })
+    } catch(err) {
+        console.log('err in getting tasks: ', err)
+    }
 })
 
 module.exports = router
